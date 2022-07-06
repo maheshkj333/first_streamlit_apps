ed23e8a5ef43b70067c1310867aa2770d83fdaa7
maheshkj333 <53592989+maheshkj333@users.noreply.github.com>
2022-07-06 11:18:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner)
+streamlit.title('My parents new healthy diner')
 streamlit.header( 'Breakfast Menu')
 streamlit.text('Spinach','Brocoli')
 streamlit.text('Hard boil free range egg')
