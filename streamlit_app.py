f3ebcba2d920b4bb56741ca7f99bf54adee5d608
maheshkj333 <53592989+maheshkj333@users.noreply.github.com>
2022-07-06 11:34:55 +0530

    Update streamlit_app.py
    Adding fruit header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 avacado toast')
 
+
+streamlit.header('Build your own fruit smoothie')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
