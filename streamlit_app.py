f09949bebff698dfe4918ae0e80bfdcdd7ec1ed0
maheshkj333 <53592989+maheshkj333@users.noreply.github.com>
2022-07-06 11:15:36 +0530

    Update streamlit_app.py
    Adding menu in 2nd commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My parents new healthy diner'
+               'Breakfast Menu',
+               'Spinach','Brocoli',
+               'Hard boil free range egg')
